21bebadbd99007a16768289bb0ecbcb6eb362f6d
אילון <eiloncap@gmail.com>
2021-04-10 18:10:13 +0300

    Implemented CalculateRootsService algorithm & broadcast

--- app/src/main/java/exercise/find/roots/CalculateRootsService.java
+++ app/src/main/java/exercise/find/roots/CalculateRootsService.java
@@ -7,19 +7,46 @@ import android.util.Log;
 public class CalculateRootsService extends IntentService {
 
 
-  public CalculateRootsService() {
-    super("CalculateRootsService");
-  }
-
-  @Override
-  protected void onHandleIntent(Intent intent) {
-    if (intent == null) return;
-    long timeStartMs = System.currentTimeMillis();
-    long numberToCalculateRootsFor = intent.getLongExtra("number_for_service", 0);
-    if (numberToCalculateRootsFor <= 0) {
-      Log.e("CalculateRootsService", "can't calculate roots for non-positive input" + numberToCalculateRootsFor);
-      return;
+    public CalculateRootsService() {
+        super("CalculateRootsService");
     }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent == null) return;
+        Intent broadcast = new Intent();
+        long timeStartMs = System.currentTimeMillis();
+        long numberToCalculateRootsFor = intent.getLongExtra("number_for_service", 0);
+        broadcast.putExtra("original_number", numberToCalculateRootsFor);
+
+        if (numberToCalculateRootsFor <= 0) {
+            Log.e("CalculateRootsService", "can't calculate roots for non-positive input" + numberToCalculateRootsFor);
+            return;
+        }
+
+
+        for (long i = 2; i <= (long) Math.sqrt(numberToCalculateRootsFor); ++i) {
+            long timePassed = System.currentTimeMillis() - timeStartMs;
+            if (timePassed >= 20) {
+                broadcast.setAction("stopped_calculations");
+                broadcast.putExtra("original_number", numberToCalculateRootsFor);
+                broadcast.putExtra("time_until_give_up_seconds", timePassed);
+                this.sendBroadcast(broadcast);
+                return;
+            }
+            if (numberToCalculateRootsFor % i == 0) {
+                long j = numberToCalculateRootsFor / i;
+                broadcast.setAction("found_roots");
+                broadcast.putExtra("root1", i);
+                broadcast.putExtra("root2", j);
+                this.sendBroadcast(broadcast);
+                return;
+            }
+        }
+        broadcast.setAction("found_roots");
+        broadcast.putExtra("root1", numberToCalculateRootsFor);
+        broadcast.putExtra("root2", 1);
+        this.sendBroadcast(broadcast);
     /*
     TODO:
      calculate the roots.
@@ -41,5 +68,5 @@ public class CalculateRootsService extends IntentService {
        for input "829851628752296034247307144300617649465159", after 20 seconds give up
 
      */
-  }
+    }
 }
